78e55c541915c65965148b42faabce89d471e874
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-11 10:53:55 -0500

    Cambio de nombre

---
+++ Ejercicio63.py
@@ -0,0 +1,14 @@
+num1=int(input("Ingrese el primer número: "))
+num2=int(input("Ingrese el segundo número: "))
+num3=int(input("Ingrese el tercer número: "))
+num4=int(input("Ingrese el cuarto número: "))
+num5=int(input("Ingrese el quinto número: "))
+num6=int(input("Ingrese el sexto número: "))
+num7=int(input("Ingrese el séptimo número: "))
+num8=int(input("Ingrese el octavo número: "))
+num9=int(input("Ingrese el noveno número: "))
+num10=int(input("Ingrese el décimo número: "))
+suma=(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
+prom=suma/10
+print("La suma de todos los números es:",suma,"y su promedio es:",prom)
+

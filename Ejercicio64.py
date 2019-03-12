d3df3a916b47c2c48173594ed91829f963a2835f
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-12 07:23:23 -0500

    TallerPracticaProgramacion

---
+++ Ejercicio64.py
@@ -0,0 +1,10 @@
+
+n=int(input("Ingrese un número: "))
+for i in range(0,n):
+	suma=sum(range(0,n))
+	
+print("La suma de los números es:",suma)
+print("El promedio de los números es:", (suma/n))
+	
+	
+
